90c099f8529d6e472928c027ce98fab89aea50d7
TND195\T.N <truongnhudung95@gmail.com>
2016-10-27 00:11:24 +0700

    Không khác gì lần 1 đâu.thử thôi :v

--- CityTravel/app/src/main/java/edu/hcmus/minhdat/citytravel/activities/ResetPassword.java
+++ CityTravel/app/src/main/java/edu/hcmus/minhdat/citytravel/activities/ResetPassword.java
@@ -1,16 +1,27 @@
 package edu.hcmus.minhdat.citytravel.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import edu.hcmus.minhdat.citytravel.R;
 
 public class ResetPassword extends AppCompatActivity {
 
-
+    Button bt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reset_password);
+        bt = (Button)findViewById(R.id.btnResetPassword);
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent a = new Intent(ResetPassword.this,LoginActivity.class);
+                startActivity(a);
+            }
+        });
     }
 }
