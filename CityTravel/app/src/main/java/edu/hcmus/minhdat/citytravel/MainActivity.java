d2f370e18bdced623315baee798786c4d9b0c67a
MinhDat <haminhdat01@gmail.com>
2016-10-27 03:00:35 +0700

    Screen design
    + Màn hình chi tiết dịch vụ.
    + Màn hình chính.

---
+++ CityTravel/app/src/main/java/edu/hcmus/minhdat/citytravel/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.hcmus.minhdat.citytravel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ CityTravel/app/src/main/java/edu/hcmus/minhdat/citytravel/activities/ResetPassword.java
@@ -0,0 +1,16 @@
+package edu.hcmus.minhdat.citytravel.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import edu.hcmus.minhdat.citytravel.R;
+
+public class ResetPassword extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_password);
+    }
+}
