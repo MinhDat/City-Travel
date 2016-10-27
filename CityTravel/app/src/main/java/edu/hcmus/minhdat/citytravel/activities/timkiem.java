1a6f340adb91eb93193b413d582aec91387071e9
DESKTOP-J2R4JMB\Huy <nguyenlinhvuhuy@gmail,com>
2016-10-27 13:22:25 +0700

    Tìm kiếm + Chỉnh sửa thông tin

---
+++ CityTravel/app/src/main/java/edu/hcmus/minhdat/citytravel/activities/timkiem.java
@@ -0,0 +1,41 @@
+package edu.hcmus.minhdat.citytravel.activities;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
+
+import edu.hcmus.minhdat.citytravel.R;
+
+public class timkiem extends AppCompatActivity {
+
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timkiem);
+        initToolBar();
+    }
+
+    public void initToolBar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.toolbar_seach);
+
+        setSupportActionBar(toolbar);
+
+        toolbar.setNavigationIcon(R.drawable.ic_toolbar_arrow);
+        toolbar.setNavigationOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(timkiem.this, "Clicking the toolbar!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+        );
+    }
+}
