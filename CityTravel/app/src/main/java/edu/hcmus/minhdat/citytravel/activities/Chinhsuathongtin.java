27c13c1b06df3ceb9342e0695e77a9024c3fe7c1
HB_SENPAI\HuuBao <huubao2309@gmail.com>
2016-11-06 15:51:06 +0700

    Update UI!

--- CityTravel/app/src/main/java/edu/hcmus/minhdat/citytravel/activities/Chinhsuathongtin.java
+++ CityTravel/app/src/main/java/edu/hcmus/minhdat/citytravel/activities/Chinhsuathongtin.java
@@ -19,7 +19,7 @@ public class Chinhsuathongtin extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chinhsuathongtin);
+        setContentView(R.layout.activity_edit_profile);
         initToolBar();
 
         }
