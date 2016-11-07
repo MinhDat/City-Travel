a1d408437c34acaadd7e6211db24aec4b33e1a2c
NguyenThaiAnh <thaianhkhtn@gmail.com>
2016-11-08 01:39:03 +0700

    Bottom navigation bar

--- CityTravel/app/src/main/java/com/example/thaianhit/citytravel/MainActivity.java
+++ CityTravel/app/src/main/java/com/example/thaianhit/citytravel/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.thaianhit.citytravel;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v7.app.AppCompatActivity;
+
+import com.roughike.bottombar.BottomBar;
 
 public class MainActivity extends AppCompatActivity {
 
+    private BottomBar bottomBar;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomBar = BottomBar.attachShy((CoordinatorLayout) findViewById(R.id.myCoordinator),
+                findViewById(R.id.myScrollingContent), savedInstanceState);
+        bottomBar.setItems(R.menu.bottom_bar_menu);
     }
 }
