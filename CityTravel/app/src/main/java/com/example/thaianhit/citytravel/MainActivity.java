4bbdcd71d1447619f9abdd5f38c5e5636b9bc666
TND195\T.N <truongnhudung95@gmail.com>
2016-11-08 15:10:50 +0700

    bottom phien ban 2.0

--- CityTravel/app/src/main/java/com/example/thaianhit/citytravel/MainActivity.java
+++ CityTravel/app/src/main/java/com/example/thaianhit/citytravel/MainActivity.java
@@ -1,23 +1,27 @@
 package com.example.thaianhit.citytravel;
 
 import android.os.Bundle;
+import android.support.annotation.IdRes;
 import android.support.annotation.Nullable;
-import android.support.design.widget.CoordinatorLayout;
 import android.support.v7.app.AppCompatActivity;
 
 import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnTabSelectListener;
 
 public class MainActivity extends AppCompatActivity {
 
-    private BottomBar bottomBar;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        bottomBar = BottomBar.attachShy((CoordinatorLayout) findViewById(R.id.myCoordinator),
-                findViewById(R.id.myScrollingContent), savedInstanceState);
-        bottomBar.setItems(R.menu.bottom_bar_menu);
+        BottomBar bottomBar = (BottomBar) findViewById(R.id.bottomBar);
+        bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
+            @Override
+            public void onTabSelected(@IdRes int tabId) {
+
+            }
+        });
     }
 }
