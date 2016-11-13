fa7efc5cd585a95f4dc918173536d7dd56076439
NguyenThaiAnh <thaianhkhtn@gmail.com>
2016-11-13 16:21:17 +0700

    thêm cusom dialog cho màn hình chi tiết dịch vụ

--- CityTravel/app/src/main/java/com/example/thaianhit/citytravel/FragmentAboutMe.java
+++ CityTravel/app/src/main/java/com/example/thaianhit/citytravel/FragmentAboutMe.java
@@ -6,6 +6,7 @@ import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
